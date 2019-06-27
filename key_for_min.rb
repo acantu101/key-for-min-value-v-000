8a2209f40f4c86e99926335f63363aede2fef0b0
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-27 03:52:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,25 +7,7 @@ if name_hash.size = 0
 end
 
 name_hash.map do |item, value|
-  if
 end
 item
 end
-end
-
-
-  arr.each do |n|
-    if lowest.nil? or n < lowest
-      # if we have no lowest or we found an element lower than current lowest,
-      # update our lowest and 2nd lowest
-      second_lowest = lowest
-      lowest = n
-    elsif second_lowest.nil? or n < second_lowest
-      # if we have no 2nd lowest or we found an element between lowest and 2nd
-      # lowest, update our 2nd lowest
-      second_lowest = n
-    end
-  end
-
-  return lowest, second_lowest
 end
